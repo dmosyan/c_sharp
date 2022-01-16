cfd4bb1f7a33b250a4c9724fb3ed8a48c8b6ea10
Susik <hello@Susiks-MacBook-Pro.local>
2022-01-16 18:43:24 +0400

    add data processor

---
+++ Intermediate/class_libraries/working_with_files_and_streams/DataProcessor/FileProcessor.cs
@@ -0,0 +1,16 @@
+using static System.Console;
+
+namespace DataProcessor
+{
+    class FileProcessor
+    {
+        public string InputFilePath {get;}
+
+        public FileProcessor(string filePath) => InputFilePath = filePath;
+
+        public void Process()
+        {
+            WriteLine($"Begin process of {InputFilePath}");
+        }
+    }
+}
---
+++ Intermediate/class_libraries/working_with_files_and_streams/DataProcessor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+
+namespace DataProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Parsing command line options");
+
+            var command = args[0];
+
+            if(command == "--file")
+            {
+                var filePath = args[1];
+                WriteLine($"Single file {filePath} selected");
+                ProcessSignleFile(filePath);
+            }
+            else if(command == "--dir")
+            {
+                var directoryPath = args[1];
+                var fileType = args[2];
+                WriteLine($"Directory {directoryPath} selected for {fileType} files");
+                ProcessDirectory(directoryPath, fileType);
+            }
+            else
+            {
+                WriteLine("Invalid command line options");
+            }
+            WriteLine("Press enter to quite");
+            ReadLine();
+        }
+
+        private static void ProcessSignleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            
+        }
+    }
+}
